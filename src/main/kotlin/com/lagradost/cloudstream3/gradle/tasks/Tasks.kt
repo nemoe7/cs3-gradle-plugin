84d27fbf6ff2cdbe6054e0ec25d7de6377560118
stanpai <stanpai78@gmail.com>
2025-05-18 12:58:49 +0800

    refactor: shorten adb task name

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/Tasks.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/Tasks.kt
@@ -207,7 +207,7 @@ fun registerTasks(project: Project) {
         it.group = TASK_GROUP
     }
 
-    project.tasks.register("deployWithAdb", DeployWithAdbTask::class.java) {
+    project.tasks.register("adb", DeployWithAdbTask::class.java) {
         it.group = TASK_GROUP
         it.dependsOn("make")
     }
